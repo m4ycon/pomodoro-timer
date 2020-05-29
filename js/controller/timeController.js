af18d68321471c31b03d98b922cad13dfad2cb2d
Maycon Fabio <m.vinnicyus@gmail.com>
2020-05-28 21:14:42 -0300

    play/pause function

--- js/controller/timeController.js
+++ js/controller/timeController.js
@@ -2,10 +2,22 @@ import formatTime from '../helper/formatTime.js';
 
 const clock = document.querySelector('.clock');
 
-let seconds = 0;
+const playBtn = document.querySelector('#play-btn');
+const stopBtn = document.querySelector('#stop-btn');
+const resetBtn = document.querySelector('#reset-btn');
+
+let runTime = true;
+
+playBtn.onclick = () => {
+  runTime = !runTime;
+  if (runTime) playBtn.innerHTML = '<i class="fas fa-pause"></i>'
+  else playBtn.innerHTML = '<i class="fas fa-play"></i>';
+}
+
+let seconds = 25*60;
 setInterval(function() {
+  if (runTime) seconds--;
   clock.innerHTML = formatTime(seconds);
-  seconds++;
 }, 1000);
 
 export default {};
