3d16dc1edcb4236de359387ec3e5468333005746
Maycon Fabio <m.vinnicyus@gmail.com>
2020-05-28 23:11:24 -0300

    pomodoro's times (25 5 15...)

--- js/controller/timeController.js
+++ js/controller/timeController.js
@@ -3,21 +3,48 @@ import formatTime from '../helper/formatTime.js';
 const clock = document.querySelector('.clock');
 
 const playBtn = document.querySelector('#play-btn');
-const stopBtn = document.querySelector('#stop-btn');
 const resetBtn = document.querySelector('#reset-btn');
 
-let runTime = true;
+const playIcon = '<i class="fas fa-play"></i>';
+const pauseIcon = '<i class="fas fa-pause"></i>';
+
+let runTime = false;
+let seconds = 1;
+let stage = 1; // 0: long-rest // 2,4,6:rest // 1,3,5,7:work
 
 playBtn.onclick = () => {
   runTime = !runTime;
-  if (runTime) playBtn.innerHTML = '<i class="fas fa-pause"></i>'
-  else playBtn.innerHTML = '<i class="fas fa-play"></i>';
+  if (runTime) playBtn.innerHTML = pauseIcon
+  else playBtn.innerHTML = playIcon;
 }
 
-let seconds = 25*60;
-setInterval(function() {
-  if (runTime) seconds--;
-  clock.innerHTML = formatTime(seconds);
-}, 1000);
+resetBtn.onclick = () => {
+  seconds = 25 * 60;
+  stage = 1;
+  runTime = true;
+  playBtn.innerHTML = pauseIcon;
+}
+
+setInterval(function () {
+  if (runTime && seconds >= 0) {
+
+    seconds--;
+    clock.innerHTML = formatTime(seconds);
+
+    if (seconds == 0) {
+      if (stage == 0) seconds = 15 * 60;
+      if (stage == 1 || stage == 3 || stage == 5) seconds = 25 * 60;
+      if (stage == 2 || stage == 4 || stage == 6) seconds = 5 * 60;
+      if (stage == 7) {
+        seconds = 25 * 60 + 1;
+        stage = 0;
+      } else {
+        seconds++;
+        stage++;
+      }
+    }
+
+  };
+}, 5);
 
 export default {};
--- js/helper/formatTime.js
+++ js/helper/formatTime.js
@@ -5,5 +5,8 @@ export default function formatTime(seconds) {
   if (hours < 10) hours = '0' + hours;
   if (minutes < 10) minutes = '0' + minutes;
   if (seconds < 10) seconds = '0' + seconds;
+
+  if (hours == 0 && minutes > 0) return `${minutes}:${seconds}`
+  else if (hours == 0 && minutes == 0) return `${seconds}s`;
   return `${hours}:${minutes}:${seconds}`;
 }
